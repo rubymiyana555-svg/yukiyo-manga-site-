84dd92a3c2715b56494e5cee52ebc38fd5e6d2e8
unknown <rubymiyana555@gmail.com>
2026-03-11 18:30:59 +0800

    add manga reader page

---
+++ manga/[id]/page.tsx
@@ -0,0 +1,14 @@
+export default function MangaPage() {
+  return (
+    <main style={{padding:"40px"}}>
+      <h1>📖 Manga Reader</h1>
+
+      <p>This is where manga pages will appear.</p>
+
+      <img src="/manga1/page1.jpg" style={{width:"100%"}} />
+      <img src="/manga1/page2.jpg" style={{width:"100%"}} />
+      <img src="/manga1/page3.jpg" style={{width:"100%"}} />
+
+    </main>
+  );
+}
